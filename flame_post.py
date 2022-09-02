d011a43f67aac24dcb88b424a993c0fc0bbd6e7a
221314 <107024244+221314@users.noreply.github.com>
2022-09-02 15:27:10 +0900

    Add files via upload

---
+++ flame_post.py
@@ -0,0 +1,38 @@
+import RPi.GPIO as GPIO
+import time
+import requests
+
+FLAME = 17  # BCM. 17, wPi. 0, Physical. 11(DOUT에 연결)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(FLAME, GPIO.IN)
+
+if __name__ == "__main__" :
+    try :
+        while(1) :
+
+            now = time.localtime()
+            timestamp = ("%04d-%02d-%02d %02d:%02d:%02d" % 
+            (now.tm_year, now.tm_mon, now.tm_mday, 
+            now.tm_hour, now.tm_min, now.tm_sec))
+            flame = GPIO.input(FLAME)
+
+            if flame == 1 : # 평소 1을 전송함
+                userdata = {'type':'fire_sensor', 'val':flame }
+                url = 'http://192.168.1.242/query.php'
+                resp = requests.post(url, data=userdata, verify=False)
+                print (timestamp,"SAVE",resp)
+                time.sleep(1.5)
+                
+            else :  
+                            # 불꽃 감지시 0을 전송함
+                userdata = {'type':'fire_sensor', 'val': flame }
+                url = 'http://192.168.1.242/query.php'
+                resp = requests.post(url, data=userdata, verify=False)
+                print (timestamp,"FLAME",resp)
+                time.sleep(1.5)
+    except :
+        print("err or Ctrl - C")
+    finally :
+        GPIO.cleanup()
+        print("END")
